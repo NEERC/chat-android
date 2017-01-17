2f36f12a617b7156a15bdd49a0b39a088258bfc7
Dmitry Deshevoy <mityada@gmail.com>
2017-01-18 02:59:42 +0300

    Display groups in roster

--- app/src/main/java/ru/ifmo/neerc/chat/android/RosterAdapter.java
+++ app/src/main/java/ru/ifmo/neerc/chat/android/RosterAdapter.java
@@ -3,6 +3,10 @@ package ru.ifmo.neerc.chat.android;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
+import java.util.Map;
+import java.util.Set;
+import java.util.TreeMap;
+import java.util.TreeSet;
 
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
@@ -19,7 +23,10 @@ import ru.ifmo.neerc.chat.user.UserRegistry;
 
 public class RosterAdapter extends RecyclerView.Adapter<RosterAdapter.ViewHolder> {
 
-    private final List<UserEntry> users = new ArrayList<UserEntry>();
+    private static int TYPE_USER  = 0;
+    private static int TYPE_GROUP = 1;
+
+    private final List<Object> items = new ArrayList<Object>();
 
     public class ViewHolder extends RecyclerView.ViewHolder {
         private ImageView iconView;
@@ -39,33 +46,68 @@ public class RosterAdapter extends RecyclerView.Adapter<RosterAdapter.ViewHolder
             iconView.setAlpha(user.isOnline() ? 1.0f : 0.3f);
             nameView.setText(user.getName());
         }
+
+        public void setGroup(String group) {
+            nameView.setText(group);
+        }
     }
 
     public RosterAdapter() {
     }
 
     @Override
+    public int getItemViewType(int position) {
+        Object item = items.get(position);
+
+        if (item instanceof UserEntry)
+            return TYPE_USER;
+        else if (item instanceof String)
+            return TYPE_GROUP;
+
+        throw new RuntimeException("Unsupported item type");
+    }
+
+    @Override
     public RosterAdapter.ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        final int layout = (viewType == TYPE_USER) ? R.layout.user : R.layout.group;
+
         View view = LayoutInflater.from(parent.getContext())
-            .inflate(R.layout.user, parent, false);
+            .inflate(layout, parent, false);
+
         return new ViewHolder(view);
     }
 
     @Override
     public void onBindViewHolder(ViewHolder holder, int position) {
-        UserEntry user = users.get(position);
-        holder.setUser(user);
+        Object item = items.get(position);
+        if (item instanceof UserEntry)
+            holder.setUser((UserEntry) item);
+        else if (item instanceof String)
+            holder.setGroup((String) item);
     }
 
     @Override
     public int getItemCount() {
-        return users.size();
+        return items.size();
     }
 
     public void update() {
-        users.clear();
-        users.addAll(UserRegistry.getInstance().getUsers());
-        Collections.sort(users);
+        Map<String, Set<UserEntry>> groups = new TreeMap<String, Set<UserEntry>>();
+        for (UserEntry user : UserRegistry.getInstance().getUsers()) {
+            if (!groups.containsKey(user.getGroup()))
+                groups.put(user.getGroup(), new TreeSet<UserEntry>());
+
+            groups.get(user.getGroup()).add(user);
+        }
+
+        items.clear();
+        for (String group : groups.keySet()) {
+            items.add(group);
+            for (UserEntry user : groups.get(group)) {
+                items.add(user);
+            }
+        }
+
         notifyDataSetChanged();
     }
 }
