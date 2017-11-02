e65181b43e2a4b7bf44dab69a579a9f820c19f36
Dmitry Deshevoy <mityada@gmail.com>
2017-11-02 07:15:44 +0300

    Upload APK to neerc.ifmo.ru

---
+++ scripts/deploy.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+
+SRC=app/build/outputs/apk/app-neerc-release.apk
+
+USER=volunteers
+HOST=neerc.ifmo.ru
+DEST=volunteers/chat/app-neerc-release.apk
+
+TMP=$DEST.tmp
+
+echo "Uploading artifacts..."
+scp $SRC $USER@$HOST:$TMP
+ssh $USER@$HOST mv $TMP $DEST
