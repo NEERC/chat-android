a49ba445a1e00236779324a802a3563a7896ccf8
Dmitry Deshevoy <mityada@gmail.com>
2017-11-29 23:55:48 +0300

    Don't resume connection on presence changes and clock ticks

--- app/src/main/java/ru/ifmo/neerc/chat/android/push/ChatFirebaseMessagingService.java
+++ app/src/main/java/ru/ifmo/neerc/chat/android/push/ChatFirebaseMessagingService.java
@@ -5,6 +5,10 @@ import android.util.Log;
 import com.google.firebase.messaging.FirebaseMessagingService;
 import com.google.firebase.messaging.RemoteMessage;
 
+import org.jivesoftware.smack.packet.Presence;
+import org.jivesoftware.smack.packet.Stanza;
+import org.jivesoftware.smack.util.PacketParserUtils;
+
 import ru.ifmo.neerc.chat.android.ChatService;
 
 public class ChatFirebaseMessagingService extends FirebaseMessagingService {
@@ -12,10 +16,30 @@ public class ChatFirebaseMessagingService extends FirebaseMessagingService {
 
     @Override
     public void onMessageReceived(RemoteMessage remoteMessage) {
-        Log.d(TAG, "Received message: " + remoteMessage);
+        Log.d(TAG, "Received message: " + remoteMessage.getData());
+
+        try {
+            Stanza stanza = PacketParserUtils.parseStanza(remoteMessage.getData().get("stanza"));
+            if (!shouldResumeConnection(stanza))
+                return;
+        } catch (Exception e) {
+            // ignore
+        }
 
         ChatService chatService = ChatService.getInstance();
         if (chatService != null)
             chatService.getConnectionManager().connect();
+    }
+
+    private boolean shouldResumeConnection(Stanza stanza) {
+        // Don't resume on presence changes
+        if (stanza instanceof Presence)
+            return false;
+
+        // Don't resume on clock ticks
+        if (stanza.getExtension("x", "http://neerc.ifmo.ru/protocol/neerc#clock") != null)
+            return false;
+
+        return true;
     }
 }
